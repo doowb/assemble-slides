0850b222a465b6fa138064fa3a3f7c6e4aee9c59
Brian Woodward <brian.woodward@gmail.com>
2014-08-14 21:48:53 -0400

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-slides <https://github.com/doowb/assemble-slides>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
